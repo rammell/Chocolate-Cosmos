016bbaec5a6e729f7ca2731c18bcbab840ed9d84
klauritz <klauritz@hawaii.edu>
2017-03-21 12:39:59 -1000

    finished up first draft of code

--- weatherbox.ino
+++ weatherbox.ino
@@ -9,18 +9,26 @@
 
 /* External Libraries */
 #include <SHT1x.h>
-#include <OneWire.h>
+#include <Wire.h>
 #include <Adafruit_BMP085.h>
 #include <XBee.h>
 #include <EEPROM.h>
 
+schema_1 Gpacket;
+XBee Gxbee = Xbee();
+int globalcount = 0;
 
 void setup() {
   // put your setup code here, to run once:
+  sensor_init();
+  Serial.begin(9600);
+  Gxbee.begin(Serial);
+
+  clear_packet();
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  routine(&globalcount);
 }
